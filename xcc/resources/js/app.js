4e0a1bfd7f0cdd8068a304730c769aac9c4ca426
linqiu <1131868300@qq.com>
2021-10-14 17:06:39 +0800

    change langeuage

--- xcc/resources/js/app.js
+++ xcc/resources/js/app.js
@@ -6,16 +6,43 @@ const $ = require('cash-dom')
 const _ = require('lodash');
 const Polyglot = require('node-polyglot')
 
+//语言转换
 const dicts = {
     en: {
         'connect_wallet':'Connect Wallet',
         'connect_title':'Chain Connect (%{x_symbol})',
-        'x_dep_addr':'%{x_symbol} deposit address'
+        'x_dep_addr': '%{x_symbol} deposit address',
+        'x_network_select': 'The Plot Network',
+        'x_introduction': 'introduction',
+        'x_banlance': 'Token Balance:',
+        'x_obtain': 'Obtain Address',
+        'x_pancake': 'Provide Liquidity to Pancake',
+        'x_xch_dep_his': '%{x_symbol} deposit history',
+        'x_load': 'Load',
+        'x_total': 'Total:',
+        'x_xch_with_addr': '%{x_symbol} withdraw address',
+        'x_bind_addr': 'Bind Address',
+        'x_xch_with': '%{x_symbol} Withdraw',
+        'x_withdraw': 'Withdraw',
+        'x_contact': 'Contact us'
     },
     zh: {
         'connect_wallet':'连接钱包',
         'connect_title':'连接 (%{x_symbol})',
-        'x_dep_addr':'%{x_symbol} 存入地址'
+        'x_dep_addr': '%{x_symbol} 存入地址',
+        'x_network_select': 'Plot网络选择',
+        'x_introduction': '说明',
+        'x_banlance': '代币余额:',
+        'x_obtain': '获取地址',
+        'x_pancake': 'Provide Liquidity to Pancake',
+        'x_xch_dep_his':'%{x_symbol}存储历史',
+        'x_load': '登录',
+        'x_total': '总计',
+        'x_xch_with_addr': '%{x_symbol} 取款地址',
+        'x_bind_addr': '绑定地址',
+        'x_xch_with':'%{x_symbol} 取款',
+        'x_withdraw': '取款',
+        'x_contact':'联系我们'
     }
 }
 
