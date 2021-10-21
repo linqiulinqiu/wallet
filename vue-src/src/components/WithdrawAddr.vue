945bc0b100cef4cd34ff03ad18da267bbb58fc89
linqiulinqiu <92359105+linqiulinqiu@users.noreply.github.com>
2021-10-21 17:59:42 +0800

    Add files via upload

---
+++ vue-src/src/components/WithdrawAddr.vue
@@ -0,0 +1,29 @@
+<template>
+    <el-container>
+    <el-header>
+      <el-row type="flex" align="center" justify="center">
+        <el-col :span="12"><div>Deposit</div></el-col>
+        <el-col :span="12"><div>Withdraw</div></el-col>
+      </el-row>
+    </el-header>
+    <el-main>
+      <div>
+          <el-input v-model="input" placeholder="please import the address" suffix-icon="el-icon-edit"></el-input>
+          <el-button>Bind address</el-button>
+          <p>Binding,please wait.</p>
+      </div>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+
+export default {
+  
+  data() {
+    return {
+      input: ''
+    }
+  }
+}
+</script>
---
+++ vue-src/src/components/WithdrawBurn.vue
@@ -0,0 +1,32 @@
+<template>
+    <el-container>
+        <el-header>
+            <el-row type="flex" align="center" justify="center">
+                <el-col :span="12">
+                    <div>Deposit</div>
+                </el-col>
+                <el-col :span="12">
+                    <div>Withdrow</div>
+                </el-col>
+            </el-row>
+        </el-header>
+        <el-main>
+            <div>
+                <p>addr</p>
+                <el-input type='text' placeholder="please enter the amount." suffix-icon="el-icon-edit"></el-input>
+                <p>You'will receive <span>xx</span> </p>
+                <el-button>Withdraw</el-button>
+            </div>
+        </el-main>
+    </el-container>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                input: ''
+            }
+        }
+    }
+</script>
