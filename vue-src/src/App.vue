c6e65be7ce49fbd8391ca63f9c97cebb027093ed
linqiu <1131868300@qq.com>
2021-10-22 19:23:39 +0800

    add tab to change deposit and withdraw

--- vue-src/src/App.vue
+++ vue-src/src/App.vue
@@ -1,40 +1,49 @@
 <template>
   <div id="app">
     <el-container>
-      <el-head>
+      <el-header>
         <el-row :gutter="20" type="flex" align="center">
     <!-- logo部分 -->
-          <el-col :span='4'>
-            <div>logo</div>
+          <el-col :span='4' id="logo">
             </el-col>
     <!-- 余额部分 -->
-          <el-col :span='1'>版本号:10/21-1</el-col>
+          <el-col :span='1'>版本号:10/22-1</el-col>
           <el-col :span='1' :offset="13">
-            <div>banlance</div>
+            <div id="token-balance">
+              banlance: <span id="token-balance">余额</span></div>
           </el-col>
     <!-- 账户部分 -->
           <el-col :span='3'>
-            <div>user</div>
+            <div id="user">user</div>
           </el-col>
     <!-- 语言部分 -->
           <el-col :span='3'>
-            <div>language</div>
+            <el-select placeholder="set language" id='setLanguage'>
+              <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              ></el-option>
+            </el-select>
           </el-col>
         </el-row>
-      </el-head>
+      </el-header>
 
-      <el-main>
+      <el-main class="mainBody">
         <el-row type="flex" align="center" justify="center">
           <el-col :span='8'>
   <!-- 链接钱包 -->
           <ConnectWallet />
   <!-- 存款地址 -->
-          <DepositAddr />
+          <Tab />
+          <!-- <DepositAddr /> -->
   <!-- 绑定地址 -->
-          <WithdrawAddr />
+          <!-- <WithdrawAddr />  -->
   <!-- 取款金额 -->
-          <WithdrawBurn />
+          <!-- <WithdrawBurn /> -->
           </el-col>
+          
         </el-row>
       </el-main>
 
@@ -52,17 +61,33 @@
 
 <script>
 import ConnectWallet from './components/ConnectWallet.vue'
-import DepositAddr from './components/DepositAddr.vue'
-import WithdrawAddr from './components/WithdrawAddr.vue'
-import WithdrawBurn from './components/WithdrawBurn.vue'
+// import DepositAddr from './components/DepositAddr.vue'
+// import WithdrawAddr from './components/WithdrawAddr.vue'
+// import WithdrawBurn from './components/WithdrawBurn.vue'
+
+import Tab from './components/Tab.vue'
 
 export default {
   name: 'App',
   components: {
     ConnectWallet,
-    DepositAddr,
-    WithdrawAddr,
-    WithdrawBurn
+    // DepositAddr,
+    // WithdrawAddr,
+    // WithdrawBurn,
+    Tab
+  },
+  // 语言设置
+  value:'100',
+  data(){
+    return{
+      options:[{
+        value:'language 1',
+        label:'English'
+      },{
+        value:'language 2',
+        label:'简体中文'
+      }]
+    }
   }
 }
 </script>
@@ -91,6 +116,16 @@ export default {
     background-color: #E9EEF3;
     color: #333;
     text-align: center;
+  }
+  /* logo */
+  #logo{
+    background:url("./assets/images/logo-bridge.png") no-repeat center;
+    background-size: 100% 200%;
+  }
+  /* 页面主体部分 */
+  
+  .mainBody>.el-row{
+    height: 500px;
   }
   
 </style>
