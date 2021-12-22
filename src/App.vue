fbd073618f84b1a28617d68957d72aa8e6fe7004
linqiu <1131868300@qq.com>
2021-12-22 18:10:42 +0800

    add walletInfo.vue

--- src/App.vue
+++ src/App.vue
@@ -3,27 +3,21 @@
     <el-row :gutter="10" type="flex" justify="center" align="middle">
       <el-col :xs="20" :sm="18" :md="14" :lg="10" :xl="10">
         <el-col class="a"><Connect /></el-col>
-        <!-- <el-col class="a"><Mnemonic /></el-col> -->
-        <!-- <el-col class="aa"><History /></el-col> -->
-        <!-- <el-col class="aa"><Send /></el-col> -->
+        <el-col class="a"><Wallet-info /></el-col>
       </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
-// import Mnemonic from "./components/Mnemonic.vue";
-// import Send from "./components/Send.vue";
-// import History from "./components/History.vue";
+import WalletInfo from "./components/WalletInfo.vue";
 import Connect from "./components/ConnectWallet.vue";
 
 export default {
   name: "App",
   components: {
-    // Send,
-    // History,
-    // Mnemonic,
     Connect,
+    WalletInfo,
   },
 };
 </script>,
@@ -42,5 +36,4 @@ export default {
   width: 700px;
   margin: 50px auto;
 }
-
 </style>
---
+++ src/components/WalletInfo.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-col>
+    <el-col id="w-info">
+      <el-button size="mini" class="el-icon-close" type="primary"></el-button>
+      <el-button size="mini" type="primary">info</el-button>
+    </el-col>
+    <p>Receive Address:</p>
+    <el-col class="ss"> </el-col>
+    <el-col>
+      <p>Acount Balance: <span>{{}}</span></p>
+      <p>Spendable: <span>{{}}</span></p>
+    </el-col>
+    <el-col v-if="false">
+      <el-input type="text" style="width: 80%"></el-input>
+      <el-button>Send</el-button> <br />
+      <el-input type="text" style="width: 200px"></el-input>
+      <span>Fee/Amount</span>
+    </el-col>
+  </el-col>
+</template>
